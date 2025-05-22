92435e8ed1008245ed1f955266c12473357d2c04
C4ller <104850578+C4ller@users.noreply.github.com>
2025-05-22 16:17:17 -0400

    Add files via upload

---
+++ Pet Adoption website/public/js/petSite.js
@@ -0,0 +1,257 @@
+function getCurrentDate(){
+    let now = new Date();
+
+    const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let weekday = weekdays[now.getDay()];
+
+    let day = now.getDate();
+
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    let month = months[now.getMonth()];
+
+    let year = now.getFullYear();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    if(seconds<10){
+        seconds = "0"+seconds;
+    }
+    if(minutes<10){
+        minutes = "0"+minutes;
+    }
+    if(hours<10){
+        hours = "0"+hours;
+    }
+    
+    document.getElementById("date").innerHTML = weekday+", "+month+" "+day+", "+year+"<br>"+hours+": "+minutes+": "+seconds;
+    
+}
+
+function checkFieldsAdopt(e){
+    //string for easier error message
+    let err="";
+
+    //checking animals radio button
+   let animalsCHecked = document.getElementsByName("Animal");
+   let isAChecked = false;
+   for(let i=0;i<animalsCHecked.length;i++) {
+        if (animalsCHecked[i].checked) {
+            isAChecked = true;
+            let animal = animalsCHecked[i].value;
+            break;
+        }
+    }
+
+    //checking breed checkboxes
+    let breedCHecked = document.querySelectorAll(".breed");
+    let isBChecked = false;
+    for(let i=0;i<breedCHecked.length;i++) {
+        if (breedCHecked[i].checked) {
+            isBChecked = true;
+            break;
+        }
+    }
+
+    //checking gender radio button
+   let genderCHecked = document.getElementsByName("Gender");
+   let isGChecked = false;
+   for(let i=0; i < genderCHecked.length; i++) {
+        if (genderCHecked[i].checked) {
+            isGChecked = true;
+            break;
+        }
+    }
+
+    //checking behavior checkboxes
+    let niceCHecked = document.querySelectorAll(".niceOrnot");
+    let isNChecked = false;
+    for(let i=0;i<niceCHecked.length;i++) {
+        if (niceCHecked[i].checked) {
+            isNChecked = true;
+            break;
+        }
+    }
+
+    if (!isAChecked)
+    {
+        err += "You didn't select an animal type!\n";
+    }
+    if(!isBChecked)
+    {
+        err += "You need to select at least one breed!\n";
+    }
+    if(!isGChecked)
+    {
+        err += "You didn't select a gender!\n";
+    }
+    if(!isNChecked)
+    {
+        err += "You need to select at least one behavior need!";
+    }
+    if(err){
+        //prevents the page from refreshing
+        e.preventDefault();
+
+        window.alert(err);
+    }
+
+    return true;
+}
+
+function checkFieldsGive(e){
+    //string for easier error message
+    let err="";
+
+    //checking animals radio button
+   let animalsCHecked = document.getElementsByName("Animal");
+   let isAChecked = false;
+   for(let i=0;i<animalsCHecked.length;i++) {
+        if (animalsCHecked[i].checked) {
+            isAChecked = true;
+            let animal=animalsCHecked[i].value;
+            break;
+        }
+    }
+
+    //checking breed text box
+    let breed = document.getElementById("breed").value;
+    let isBChecked = false;
+    if(breed && /[\w\s]/.test(breed)) {
+        isBChecked = true;
+    }
+
+    //checking age selection in case it was left at default
+   let ageCheck = document.getElementById("age").value;
+   let isAgeChecked = false;
+   if (ageCheck != "default") isAgeChecked = true;
+
+   //checking gender radio button
+   let genderCHecked = document.getElementsByName("Gender");
+   let isGChecked = false;
+   for(let i=0; i < genderCHecked.length; i++) {
+        if (genderCHecked[i].checked) {
+            isGChecked = true;
+            let gender = genderCHecked[i].value;
+            break;
+        }
+    }
+
+    //checking behavior checkboxes
+    let niceCHecked = document.querySelectorAll(".niceOrnot");
+    let isNChecked = false;
+    let nicestr = "";
+    for(let i=0;i<niceCHecked.length;i++) {
+        if (niceCHecked[i].checked) {
+            isNChecked = true;
+            nicestr += genderCHecked[i].value+",";
+        }
+    }
+    if(isNChecked){
+       const niceArr = nicestr.split(",");
+    } 
+
+    //Checking for a description
+    let description = document.getElementById("petDescription").value;
+    let isDChecked = false;
+    if(description) {
+        isDChecked = true;
+    }
+
+    //checking the person's name text box
+    let name = document.getElementById("Name").value;
+    let isNameChecked = false;
+    if(name && /[\w\s']/.test(name)) {
+        isNameChecked = true;
+    }
+
+    //checking the email
+    let email = document.getElementById("email").value;
+    let isEChecked = false;
+    if(email && /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) isEChecked = true;
+
+    if (!isAChecked)
+    {
+        err += "You didn't select an animal type.\n";
+    }
+    if(!isBChecked)
+    {
+        err += "The breed you entered is invalid.\n";
+    }
+    if(!isAgeChecked)
+    {
+        err += "Please select the age range of the pet from the dropdown menu\n";
+    }
+    if(!isGChecked)
+    {
+        err += "You didn't select a gender.\n";
+    }
+    if(!isNChecked)
+    {
+        err += "You didn't specify at least one of the good behaviors.\n";
+    }
+    if(!isDChecked)
+    {
+        err += "Please say at least a few words about the animal you are giving away. It will really help!\n";
+    }
+    if(!isNameChecked)
+    {
+        err += "Don't forget to give us your name! If you did add it and this is appearing please try without special characters.\n";
+    }
+    if(!isEChecked)
+    {
+        err += "The email you input is invalid.";
+    }
+    if(err){
+        //prevents the page from refreshing
+        e.preventDefault();
+
+        window.alert(err);
+        return;
+    }
+
+    fetch('/newPet', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ animal, breed, ageCheck, gender, niceArr, description, name, email })
+    })
+}
+
+function checkUser(e, route){
+    //string for easier error message
+    let err="";
+
+   let username = document.getElementById("username").value;
+   let isUChecked = false;
+   if(/^[a-zA-Z0-9]+$/.test(username))
+    {
+    isUChecked=true;
+   }
+
+    let password = document.getElementById("password").value;
+    let isPChecked = false;
+    if(/^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z0-9]{4,}$/.test(password))
+    {
+        isPChecked=true;
+    }
+
+    if (!isUChecked)
+    {
+        err += "Incorrect Username format entered.\n";
+    }
+    if(!isPChecked)
+    {
+        err += "Incorrect password format entered.\n";
+    }
+    if(err){
+        window.alert(err);
+        return;
+    }
+
+    fetch(route, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password })
+    })
+}
+
+setInterval(getCurrentDate,1000);
